61d055d160ad7aba0300831d8517310816b08957
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-10-27 01:58:55 +0530

    solve LeetCode.#191_NumberOf1Bits

---
+++ LeetCode/NumberOf1Bits/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ctr = 0
+        # repetitive division by 2
+        while n != 0:
+            d = n % 2
+            if d == 1: ctr += 1 
+            n /= 2
+        
+        return ctr
