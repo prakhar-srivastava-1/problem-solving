764e8227ae214cc1af780b2608d3092f88f3589d
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2024-08-15 05:55:33 +0530

    solve LeetCode.#509_FibonacciNumber

---
+++ LeetCode/FibonacciNumbers/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        """Computes the nth Fibonacci number
+
+        Args:
+            n (int): nth term of Fibonacci series
+
+        Returns:
+            int: Fibonacci Number
+        """
+        # if n = 0, 1, 2 => 1   
+        if n in (0, 1, 2):
+            return 1
+        
+        # else F(n) = F(n-1) + F(n-2)     
+        return self.fib(n-1) + self.fib(n-2)
