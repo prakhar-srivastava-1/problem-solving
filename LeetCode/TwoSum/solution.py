850167244ffb950e37468111daf19c6047d6c33d
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2024-08-24 03:32:46 +0530

    add comments LeetCode.#1_TwoSum

--- LeetCode/TwoSum/solution.py
+++ LeetCode/TwoSum/solution.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        # memo to save the seen numbers
         complement = {}
 
         for index, item in enumerate(nums):
+            # if number is found
             if complement.get(item, (-1, False))[1]:
                 return [complement.get(item)[0], index]
+            # otherwise add to the memo
             complement[target - item] = (index, True)
